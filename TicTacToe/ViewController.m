4a9f8b79faf70b00fd410067549a12dda810119c
its-micah <mlanier86@gmail.com>
2015-03-12 18:00:57 -0500

    location finder done

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -19,8 +19,9 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelEight;
 @property (weak, nonatomic) IBOutlet UILabel *labelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
-@property CGPoint point;
+@property UILabel *pickedLabel;
 @property NSArray *labelArray;
+@property NSInteger player;
 
 
 @end
@@ -30,30 +31,59 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.labelArray = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
-
+    self.player = 0;
 }
 
 
 
-- (void)findLabelUsingPoint:(CGPoint)point{
+- (void)findLabelUsingPoint:(CGPoint)point {
 
- 
-
-    CGRectContainsPoint(self.labelOne.frame, self.labelOne.center);
-
-//if we get cgpoint of touched location, if the tapped location cgpoint is equal to the center of the label than action
-
-   
+    if (CGRectContainsPoint(self.labelOne.frame, point)) {
+        self.pickedLabel = self.labelOne;
+    } else if (CGRectContainsPoint(self.labelTwo.frame, point)) {
+        self.pickedLabel = self.labelTwo;
+    } else if (CGRectContainsPoint(self.labelThree.frame, point)) {
+        self.pickedLabel = self.labelThree;
+    } else if (CGRectContainsPoint(self.labelFour.frame, point)) {
+        self.pickedLabel = self.labelFour;
+    } else if (CGRectContainsPoint(self.labelFive.frame, point)) {
+        self.pickedLabel = self.labelFive;
+    } else if (CGRectContainsPoint(self.labelSix.frame, point)) {
+        self.pickedLabel = self.labelSix;
+    } else if (CGRectContainsPoint(self.labelSeven.frame, point)) {
+        self.pickedLabel = self.labelSeven;
+    } else if (CGRectContainsPoint(self.labelEight.frame, point)) {
+        self.pickedLabel = self.labelEight;
+    } else if (CGRectContainsPoint(self.labelNine.frame, point)) {
+        self.pickedLabel = self.labelNine;
+    }
 }
 
-- (IBAction)onLabelTapped:(UIGestureRecognizer *)gestureRecognizer
-{
-    CGPoint tapped = [gestureRecognizer locationInView:[self.view]];
+
+
+
+- (void)updatePlayerInfo {
+
+    if (self.player == 0) {
+        self.pickedLabel.text = @"x";
+        self.pickedLabel.textColor = [UIColor blueColor];
+    }
+    if (self.player == 1) {
+        self.pickedLabel.text = @"O";
+        self.pickedLabel.textColor = [UIColor redColor];
+    }
+
+}
+
+
+- (IBAction)onLabelTapped:(UIGestureRecognizer *)gestureRecognizer {
+    //CGPoint tapped = [gestureRecognizer locationInView:self.view];
     gestureRecognizer.delegate = self;
     if (gestureRecognizer.state == UIGestureRecognizerStateRecognized) {
-        if (CGRectContainsPoint(self.labelOne.frame, self.labelOne.center)) {
-            self.labelOne.text = @"x";
-        }
+        [self findLabelUsingPoint:[gestureRecognizer locationInView:self.view]];
+        [self updatePlayerInfo];
+
+
 
 
     }

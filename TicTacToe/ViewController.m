f5030c342dea2f05c068a88b6d4b50e87a44fcab
its-micah <mlanier86@gmail.com>
2015-03-13 15:33:52 -0500

    add new game method

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController () <UIGestureRecognizerDelegate>
+@interface ViewController () <UIGestureRecognizerDelegate, UIAlertViewDelegate>
 @property (weak, nonatomic) IBOutlet UILabel *labelOne;
 @property (weak, nonatomic) IBOutlet UILabel *labelTwo;
 @property (weak, nonatomic) IBOutlet UILabel *labelThree;
@@ -20,9 +20,11 @@
 @property (weak, nonatomic) IBOutlet UILabel *labelNine;
 @property (weak, nonatomic) IBOutlet UILabel *whichPlayerLabel;
 @property UILabel *pickedLabel;
-@property NSArray *labelArray;
+//@property NSArray *labelArray;
 @property BOOL player;
 @property NSString *winner;
+@property UIAlertView *alertView;
+
 
 
 @end
@@ -33,8 +35,27 @@
     [super viewDidLoad];
     //self.labelArray = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
     self.player = YES;
+    self.whichPlayerLabel.text = @"X";
 }
 
+
+- (void)newGame {
+
+    self.player = YES;
+    self.labelOne.text = @"";
+    self.labelTwo.text = @"";
+    self.labelThree.text = @"";
+    self.labelFour.text = @"";
+    self.labelFive.text = @"";
+    self.labelSix.text = @"";
+    self.labelSeven.text = @"";
+    self.labelEight.text = @"";
+    self.labelNine.text = @"";
+    self.winner = @"";
+    self.whichPlayerLabel.text = @"X";
+
+
+}
 
 
 - (void)findLabelUsingPoint:(CGPoint)point {
@@ -83,14 +104,75 @@
         [self findLabelUsingPoint:[gestureRecognizer locationInView:self.view]];
         [self updatePlayerInfo];
         [self whoWon];
+        if ([self.winner isEqualToString:@"X"]) {
+            self.alertView = [[UIAlertView alloc] initWithTitle:@"WahOO!"
+                                                        message:@"X is the winner"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"New Game", nil];
+
+            [self.alertView show];
+        } else if ([self.winner isEqualToString:@"O"]) {
+            self.alertView = [[UIAlertView alloc] initWithTitle:@"WahOO!"
+                                                        message:@"O is the winner"
+                                                       delegate:self
+                                              cancelButtonTitle:nil
+                                              otherButtonTitles:@"New Game", nil];
+            [self.alertView show];
+            self.whichPlayerLabel.text = @"X";
+        }
+    }
+}
+
+- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
+    if (buttonIndex == 0) {
+        [self updatePlayerInfo];
+        [self newGame];
     }
 }
 
 - (NSString *)whoWon {
-    if ([self.labelOne.text isEqualToString:self.labelTwo.text]) {
+    if (([self.labelOne.text isEqualToString:self.labelTwo.text]) &&
+        [self.labelTwo.text isEqualToString:self.labelThree.text] &&
+        (![self.labelOne.text isEqualToString:@""]))
+    {
+        self.winner = self.labelOne.text;
+    } if (([self.labelFour.text isEqualToString:self.labelFive.text]) &&
+               [self.labelFive.text isEqualToString:self.labelSix.text] &&
+        (![self.labelFour.text isEqualToString:@""]))
+    {
+        self.winner = self.labelFour.text;
+    } if (([self.labelSeven.text isEqualToString:self.labelEight.text]) &&
+        [self.labelEight.text isEqualToString:self.labelNine.text] &&
+               (![self.labelSeven.text isEqualToString:@""]))
+    {
+        self.winner = self.labelSeven.text;
+    } if (([self.labelOne.text isEqualToString:self.labelFour.text]) &&
+               [self.labelFour.text isEqualToString:self.labelSeven.text] &&
+               (![self.labelSeven.text isEqualToString:@""]))
+    {
+        self.winner = self.labelOne.text;
+    } if (([self.labelTwo.text isEqualToString:self.labelFive.text]) &&
+               [self.labelFive.text isEqualToString:self.labelEight.text] &&
+               (![self.labelFive.text isEqualToString:@""]))
+    {
         self.winner = self.labelTwo.text;
+    } if (([self.labelThree.text isEqualToString:self.labelSix.text]) &&
+               [self.labelSix.text isEqualToString:self.labelNine.text] &&
+               (![self.labelSix.text isEqualToString:@""]))
+    {
+        self.winner = self.labelThree.text;
+    } if (([self.labelOne.text isEqualToString:self.labelFive.text]) &&
+               [self.labelFive.text isEqualToString:self.labelNine.text] &&
+               (![self.labelFive.text isEqualToString:@""]))
+    {
+        self.winner = self.labelOne.text;
+    } if (([self.labelThree.text isEqualToString:self.labelFive.text]) &&
+               [self.labelFive.text isEqualToString:self.labelSeven.text] &&
+               (![self.labelFive.text isEqualToString:@""]))
+    {
+        self.winner = self.labelThree.text;
     }
-    NSLog(@"%@", self.winner);
     return self.winner;
 }
 
